c64479b043ad1978b51fd069ec7b9d815198c390
Sven Kummetz <sven.kummetz@mindscreen.de>
2026-02-11 14:05:39 +0100

    fix lang tags

--- src/elements/Violation/Violation.tsx
+++ src/elements/Violation/Violation.tsx
@@ -208,19 +208,19 @@ export class Violation extends BaseHTMLElement<IViolationAccessor> implements IV
             <Accordion id='container' onChange={handleFoldChange}>
                 <div id='heading' slot={Accordion.slots.heading}>
                     <div>
-                        <h2 id='title' ref={this.titleRef}></h2>
+                        <h2 id='title' ref={this.titleRef} lang="en"></h2>
                         <div>
                             <label id='impact' ref={this.impactRef}></label>
                         </div>
                     </div>
                 </div>
-                <div id='description' ref={this.descriptionRef}></div>
+                <div id='description' ref={this.descriptionRef} lang="en"></div>
                 <h3 ref={this.highlightTitleRef}>i18n('baat.violation.highlightError')</h3>
                 <ol id='nodeListButtons' ref={this.nodeListButtonsRef}></ol>
                 <h3 ref={this.otherTitleRef}></h3>
                 <ul id='nodeListText' ref={this.nodeListTextRef}></ul>
                 <div id='link' ref={this.linkRef}></div>
-                <p id='info' ref={this.infoRef}></p>
+                <p id='info' ref={this.infoRef} lang="en"></p>
                 <button id='hideButton' onClick={handleHide}>
                     <Icon width="16" height="16"><g fill="currentColor" stroke="none"><path d="m46 2.74c0.88 0.882 0.73 2.14 0.3 2.66l-8.27 8.15c3.25 2.04 6.19 4.53 8.79 7.35 1.5 1.68 1.5 4.22 0 5.91-5.23 5.76-13.9 11.8-22.6 11.8h-0.61c-3.03-0.1-6.03-0.7-8.79-1.93l-8.45 8.49c-0.689 0.67-1.76 0.79-2.57 0.24-1.12-0.67-1.19-2.05-0.701-2.51l39.8-39.8c0.68-0.777 2.18-1.28 3.07-0.398zm-14 17.2c-0.3 0-0.34 0-0.46 0.16l-11.4 11.4c-0.12 0-0.18 0.3-0.15 0.46 0 0.3 0.12 0.3 0.27 0.36 1.99 0.92 4.26 1.04 6.34 0.4 2.94-0.95 5.21-3.24 6.13-6.18 0.61-2.07 0.49-4.29-0.43-6.25 0-0.15-0.31-0.24-0.37-0.28z"/><path d="m15.1 26.7c0.13-0.13 0.17-0.31 0.12-0.49-0.22-0.77-0.35-1.56-0.35-2.37 0-5.08 4.1-9.18 9.18-9.18 0.8 0 1.6 0.12 2.37 0.35 0.18 0.1 0.36 0 0.49-0.12l3.96-3.95c0-0.12 0.3-0.31 0-0.49 0-0.18 0-0.31-0.34-0.37-2.01-0.691-4.26-1.03-6.49-1.02-8.79-0.119-17.5 6.03-22.8 11.9-1.53 1.67-1.54 4.22 0 5.91 2.11 2.31 4.47 4.38 7.01 6.19h0.665l6.22-6.22z"/></g></Icon>
                     i18n('baat.violation.hideIssue')
